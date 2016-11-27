d4c6d93b650294aca8686948e542e23d807d834a
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-11-26 17:01:46 -0800

    navbar ul color changed

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
       //nav bar to stick.
       console.log($(window).scrollTop())
     if ($(window).scrollTop() > 280) {
-      $('#nav_bar').addClass('navbar-fixed');
+      $('#navbar').addClass('navbar-fixed');
     }
     if ($(window).scrollTop() < 281) {
-      $('#nav_bar').removeClass('navbar-fixed');
+      $('#navbar').removeClass('navbar-fixed');
     }
   });
 });
