b8311e277c2422d6018595e897f618786524aefc
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-11-26 19:12:29 -0800

    transition added to anchors

---
+++ app/assets/javascripts/anchor.js
@@ -0,0 +1,23 @@
+$(document).on('click', 'a[href*="#"]:not([href="#"])', function(e) {
+
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+
+        var target = $(this.hash);
+
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+        if (target.length) {
+
+            $('html, body').animate({
+
+                scrollTop: target.offset().top
+
+            }, 1000);
+
+            e.preventDefault();
+
+        }
+
+    }
+
+});
